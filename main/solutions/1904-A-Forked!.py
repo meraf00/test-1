6edcca022ba129c69480d56cc20a3d232e0241a0
Leul Wujira <96904152+meraf00@users.noreply.github.com>
2023-12-31 18:02:38 +0300

    Add solution for Forked!

---
+++ main/solutions/1904-A-Forked!.py
@@ -0,0 +1,31 @@
+testcases = int(input())
+
+for _ in range(testcases):
+    a, b = map(int, input().split())
+    xk, yk = map(int, input().split())
+    xq, yq = map(int, input().split())
+
+    possible_king = set([(xk + a, yk + b),
+    (xk + a, yk - b),
+    (xk - a, yk + b),
+    (xk - a, yk - b),
+    (xk + b, yk + a),
+    (xk + b, yk - a),
+    (xk - b, yk + a),
+    (xk - b, yk - a)])
+
+    possible_queen = set([
+    (xq + a, yq + b),
+    (xq + a, yq - b),
+    (xq - a, yq + b),
+    (xq - a, yq - b),
+    (xq + b, yq + a),
+    (xq + b, yq - a),
+    (xq - b, yq + a),
+    (xq - b, yq - a)])
+
+    print(len(possible_king.intersection(possible_queen)))
+
+    
+
+    
